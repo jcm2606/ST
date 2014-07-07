5aac0831404c975172f469e699777430e2d9f63e
Jake <jakemichie97@hotmail.com>
2014-07-07 21:05:51 +1000

    Misc Changes (See desc)
    - Moved scripts back into Script folder
    - Coded a powerto script (power.txt)
    - Raises a given number to a given value
    - Changed comments in main script slightly
    - Added a /? mode for cmd
    - Added a clear-env command, to clear the current environment
    - Changed how the title system works to use variables instead of direct
    title changing
    - Changed all variables used to have the precursor "st.", to help when
    cleaning up the 'data footprint' left by ST
    - One of the necessary changes for DMCNet hooking, among other languages
    based in Batch
    - Added argument checking for the PRINT instruction (@)
    - To check if what you're trying to print is valid or not
    - Added debug output
    - Added the ability to pull data from stacks (and automatically shift
    the stack up)
    - Added checking for the system-hidden register to commands that check
    for valid registers
    - Added more conditions to IF instructions
    - Changed how number loops work
    - Allowed the END value to be a register, value will automatically be
    retrieved from the register

--- Main/st.bat
+++ Main/st.bat
@@ -3,16 +3,18 @@
 setlocal EnableDelayedExpansion
 
 REM	ST Interpreter
-REM	ST (short for ScripT) is a lightweight script interpreter written in batch.
+REM	ST (short for ScripT) is a lightweight stack-oriented script interpreter written in batch.
 REM This is the only file ST uses and requires.
 
 REM ST differentiates it's commands based on a specific set of starting strings.
 REM To use a command, you must first start the line with "::", "##" or "//".
 REM ST will recognise "::" or "#" and then take that line as a command, else it will take the line as a comment and ignore it.
 
+echo.
+
 if "%1"=="" (
-	echo ERROR: ST requires you to pass a script file as a parameter.
-	echo        For example, "st test_script.txt".
+	echo ERROR: ST requires you to pass a script file or a command as a parameter.
+	echo        For example, "st test_script.txt", or "st clear-env"
 	
 	for %%x in (%cmdcmdline%) do if /i "%%~x"=="/c" set DOUBLECLICKED=1
 	if defined DOUBLECLICKED pause
@@ -20,26 +22,93 @@ if "%1"=="" (
 	exit /b
 )
 
-set ST_SCRIPT=%1
-
-set title=ST - Script: %ST_SCRIPT%
-
-title !title!
-
-if not "%~2"=="" (
-	for %%a in (%~2) do (
-		call :add "Stack_y", "%%a"
-	)
+if "%1"=="/?" (
+	echo ST is a lightweight stack-oriented scripting language and interpreter
+	echo written entirely in Batch. This is the only binaries file
+	echo required to use ST.
+	echo.
+	echo ST is designed to work entirely from the command line, and as such
+	echo all interfacing with ST will be done via the command line.
+	echo.
+	echo To interface with ST, use the command identifier "st", or "st.bat".
+	echo.
+	echo To run a script, use the command "st [script file] {parameters OPTIONAL}".
+	echo.
+	echo If a script has errored out, and hard-crashed ST, there's a good chance
+	echo data was left over from the previous session, use the command
+	echo "st clear-env" to clear the environment of any data.
+	
+	exit /b
 )
 
-set mode_nocomment=false
+set ST_SCRIPT=%1
 
-echo.
+set st.title=ST - Script: %ST_SCRIPT%
+
+title !st.title!
+
+if "%1"=="clear-env" (
+	set st.title=ST - Clearing environment
+	
+	title !st.title!
+	
+	for /l %%a in (0, 1, !Stack_a.length!) do (
+		set "Stack_a[%%a]="
+	)
+	set "Stack_a.length="
+
+	for /l %%a in (0, 1, !Stack_b.length!) do (
+		set "Stack_b[%%a]="
+	)
+	set "Stack_b.length="
+
+	for /l %%a in (0, 1, !Stack_c.length!) do (
+		set "Stack_c[%%a]="
+	)
+	set "Stack_c.length="
+
+	for /l %%a in (0, 1, !Stack_x.length!) do (
+		set "Stack_x[%%a]="
+	)
+	set "Stack_x.length="
+
+	for /l %%a in (0, 1, !Stack_y.length!) do (
+		set "Stack_y[%%a]="
+	)
+	set "Stack_y.length="
+
+	for /l %%a in (0, 1, !Stack_z.length!) do (
+		set "Stack_z[%%a]="
+	)
+	set "Stack_z.length="
+	
+	set "st.R_a="
+	set "st.R_b="
+	set "st.R_c="
+	set "st.R_x="
+	set "st.R_y="
+	set "st.R_z="
+	set "st.st.R_h="
+	
+	exit /b
+)
+
+if not "%~2"=="" (
+	set "tempdata=%~2"
+
+	for %%a in (!tempdata!) do (
+		call :add "Stack_y", "%%a"
+	)
+	
+	set "tempdata="
+)
+
+set st.mode_nocomment=false
 
 for /F "tokens=1* delims=]" %%a in ('type "%ST_SCRIPT%" ^| find /V /N ""') do (
-	set title=ST - Script: %ST_SCRIPT% - Building script stack
+	set st.title=ST - Script: %ST_SCRIPT% - Building script stack
 
-	title !title!
+	title !st.title!
 
 	if "%%b"=="" (
 		call :add "Stack_x", "NULL_LINE"
@@ -48,101 +117,101 @@ for /F "tokens=1* delims=]" %%a in ('type "%ST_SCRIPT%" ^| find /V /N ""') do (
 	)
 )
 
-set /a line=-1
+set /a st.line=-1
 
 :readStackX
-set /a line+=1
+set /a st.line+=1
 
-for /f "tokens=1* eol=' delims= " %%a in ("!Stack_x[%line%]!") do (
-	set title=ST - Script: %ST_SCRIPT% - Line: !line!
+for /f "tokens=1* eol=' delims= " %%a in ("!Stack_x[%st.line%]!") do (
+	set st.title=ST - Script: %ST_SCRIPT% - Executing - Line: !st.line!
 
-	title !title!
+	title !st.title!
 
-	set command=%%a
-	set start=!command:~0,2!
-	set command=!command:~2!
+	set st.command=%%a
+	set start=!st.command:~0,2!
+	set st.command=!st.command:~2!
 
-	if !mode_nocomment!==true (
-		set command=%%a
+	if !st.mode_nocomment!==true (
+		set st.command=%%a
 	
-		if /i !command!==~ext (
+		if /i !st.command!==~ext (
 			exit /b
 		)
 	
-		if "!command!"=="{" (
-			set codeblock=true
+		if "!st.command!"=="{" (
+			set st.codeblock=true
 		)
 		
-		if "!command!"=="}" (
-			set "codeblock="
+		if "!st.command!"=="}" (
+			set "st.codeblock="
 		)
 		
-		if not defined codeblock (
-			call :commandHandling "!command!", "%%b"
+		if not defined st.codeblock (
+			call :commandHandling "!st.command!", "%%b"
 		)
 	) else (
 		if !start!==:: (
-			if /i !command!==~ext (
+			if /i !st.command!==~ext (
 				exit /b
 			)
 		
-			if "!command!"=="{" (
-				set codeblock=true
+			if "!st.command!"=="{" (
+				set st.codeblock=true
 			)
 			
-			if "!command!"=="}" (
-				set "codeblock="
+			if "!st.command!"=="}" (
+				set "st.codeblock="
 			)
 			
-			if not defined codeblock (
-				call :commandHandling "!command!", "%%b"
+			if not defined st.codeblock (
+				call :commandHandling "!st.command!", "%%b"
 			)
 		)
 		
 		if !start!==## (
-			if /i !command!==~ext (
+			if /i !st.command!==~ext (
 				exit /b
 			)
 		
-			if "!command!"=="{" (
-				set codeblock=true
+			if "!st.command!"=="{" (
+				set st.codeblock=true
 			)
 			
-			if "!command!"=="}" (
-				set "codeblock="
+			if "!st.command!"=="}" (
+				set "st.codeblock="
 			)
 			
-			if not defined codeblock (
-				call :commandHandling "!command!", "%%b"
+			if not defined st.codeblock (
+				call :commandHandling "!st.command!", "%%b"
 			)
 		)
 		
 		if !start!==// (
-			if /i !command!==~ext (
+			if /i !st.command!==~ext (
 				exit /b
 			)
 		
-			if "!command!"=="{" (
-				set codeblock=true
+			if "!st.command!"=="{" (
+				set st.codeblock=true
 			)
 			
-			if "!command!"=="}" (
-				set "codeblock="
+			if "!st.command!"=="}" (
+				set "st.codeblock="
 			)
 			
-			if not defined codeblock (
-				call :commandHandling "!command!", "%%b"
+			if not defined st.codeblock (
+				call :commandHandling "!st.command!", "%%b"
 			)
 		)
 	)
 )
 
-if !line! GEQ !Stack_x.length! (
+if !st.line! GEQ !Stack_x.length! (
 	call :clear "Stack_x"
 
-	set title=ST - Script: %ST_SCRIPT% - Line: !line! - Ended
+	set st.title=ST - Script: %ST_SCRIPT% - Ended - Line: !st.line!
 
-	title !title!
+	title !st.title!
 
 	exit /b
 ) else (
@@ -154,7 +223,7 @@ set cmd=%~1
 set data=%~2
 
 if /i %cmd%==~nc (
-	set mode_nocomment=true
+	set st.mode_nocomment=true
 )
 
 if /i %cmd%==~dbg (
@@ -165,19 +234,76 @@ REM	Console instructions
 
 if %cmd%==@ (
 	for %%a in (%data%) do (
+		set "valid="
 		set "tempd=%%a"
 		set "tempd_=!tempd:~1!"
 		set "tempd=!tempd:~0,1!"
 		
-		if !tempd!==. (
-			call :tostring "Stack_!tempd_!", "s"
-			set d=!tempd_!!s!
-		) else (
-			set d=!R_%%a!
+		if !tempd_!==a (
+			set valid=true
 		)
 		
-		if defined d (
-			echo !d!
+		if !tempd_!==b (
+			set valid=true
+		)
+		
+		if !tempd_!==c (
+			set valid=true
+		)
+		
+		if !tempd_!==x (
+			set valid=true
+		)
+		
+		if !tempd_!==y (
+			set valid=true
+		)
+		
+		if !tempd_!==z (
+			set valid=true
+		)
+		
+		if %%a==a (
+			set valid=true
+		)
+		
+		if %%a==b (
+			set valid=true
+		)
+		
+		if %%a==c (
+			set valid=true
+		)
+		
+		if %%a==h (
+			set valid=true
+		)
+		
+		if %%a==x (
+			set valid=true
+		)
+		
+		if %%a==y (
+			set valid=true
+		)
+		
+		if %%a==z (
+			set valid=true
+		)
+		
+		if defined valid (
+			if !tempd!==. (
+				call :tostring "Stack_!tempd_!", "s"
+				set d=!tempd_!!s!
+			) else (
+				set d=!st.R_%%a!
+			)
+			
+			if defined d (
+				echo !d!
+			) else (
+				echo ERROR IN INSTRUCTION
+			)
 		) else (
 			echo ERROR IN INSTRUCTION
 		)
@@ -196,7 +322,11 @@ if %cmd%==@_ (
 REM Script instructions
 
 if %cmd%==#~ (
-	set /a line=%data%
+	if !mode_debug!==true (
+		echo Changed line pointer from line '!st.line!' to line '%data%'
+	)
+
+	set /a st.line=%data%
 )
 
 REM	Stack instructions
@@ -289,7 +419,63 @@ if "%cmd%"=="+~" (
 		)
 		
 		if defined valid (
+			if !mode_debug!==true (
+				echo Expanded data in stack '%%c' at index '%%d' to stack '%%e'
+			)
+		
 			call :expandStack "%%c", "%%d", "%%e"
+		)
+	)
+)
+
+if "%cmd%"=="+-" (
+	for /f "tokens=1,2 delims= " %%c in ("%data%") do (
+		if %%c==a (
+			set valid=true
+		)
+		
+		if %%c==b (
+			set valid=true
+		)
+		
+		if %%c==c (
+			set valid=true
+		)
+		
+		if %%c==y (
+			set valid=true
+		)
+		
+		if defined valid (
+			set /a length=!Stack_%%c.length! - 1
+		
+			for /l %%n in (0, 1, !length!) do (
+				set data=!Stack_%%c[%%n]!
+				
+				if not %%n==%%d (
+					call :add "StorageStack", "!data!"
+				)
+			)
+			
+			call :clear "Stack_%%c"
+			
+			set "length="
+			
+			set /a length=!StorageStack.length! - 1
+			
+			for /l %%n in (0, 1, !length!) do (
+				set data=!StorageStack[%%n]!
+				
+				call :add "Stack_%%c", "!data!"
+			)
+			
+			call :clear "StorageStack"
+			
+			if !mode_debug!==true (
+				echo Removed item in stack '%%c' at index '%%d' and moved data up
+			)
+			
+			set "length="
 		)
 	)
 )
@@ -314,7 +500,7 @@ if "%cmd%"=="+." (
 			)
 			
 			if !mode_debug!==true (
-				echo Shifted and condensed stack data from '%%c' to '%%d'
+				echo Shifted and condensed stack data from stack '%%c' to stack '%%d'
 			)
 			
 			call :add "Stack_%%d", "!val!"
@@ -346,7 +532,7 @@ if "%cmd%"=="+.." (
 			)
 			
 			if !mode_debug!==true (
-				echo Shifted stack data from '%%c' to '%%d'
+				echo Shifted stack data from stack '%%c' to stack '%%d'
 			)
 			
 			set "len="
@@ -371,7 +557,7 @@ if "%cmd%"=="$@" (
 		)
 		
 		if defined valid (
-			set /p R_%%c=%%d
+			set /p st.R_%%c=%%d
 		)
 	)
 )
@@ -391,10 +577,10 @@ if "%cmd%"=="$$" (
 		)
 	
 		if defined valid (
-			set R_%%c=!R_%%d!
+			set st.R_%%c=!st.R_%%d!
 			
 			if !mode_debug!==true (
-				echo Pushed data from stack '%%e' in index '%%d' to register '%%c'
+				echo Pushed data from register '%%d' to register '%%c'
 			)
 		)
 	)
@@ -416,10 +602,10 @@ if "%cmd%"=="$." (
 			)
 		
 			if defined valid (
-				set R_%%c=!Stack_%%c[%%d]!
+				set st.R_%%c=!Stack_%%c[%%d]!
 				
 				if !mode_debug!==true (
-					echo Pushed data from stack '%%e' in index '%%d' to register '%%c'
+					echo Pushed data from stack '%%c' in index '%%d' to register '%%c'
 				)
 			)
 		) else (
@@ -434,9 +620,13 @@ if "%cmd%"=="$." (
 			if /i %%e==c (
 				set valid=true
 			)
+			
+			if /i %%e==h (
+				set valid=true
+			)
 		
 			if defined valid (
-				set R_%%e=!Stack_%%c[%%d]!
+				set st.R_%%e=!Stack_%%c[%%d]!
 				
 				if !mode_debug!==true (
 					echo Pushed data from stack '%%c' in index '%%d' to register '%%e'
@@ -462,7 +652,7 @@ if "%cmd%"=="$+" (
 		
 		if defined valid (
 			if "%%d"=="" (
-				call :add "Stack_%%c", "!R_%%c!"
+				call :add "Stack_%%c", "!st.R_%%c!"
 				
 				if !mode_debug!==true (
 					echo Pushed data from register '%%c' to stack '%%c'
@@ -481,7 +671,7 @@ if "%cmd%"=="$+" (
 				)
 			
 				if defined valid (
-					call :add "Stack_%%c", "!R_%%d!"
+					call :add "Stack_%%c", "!st.R_%%d!"
 					
 					if !mode_debug!==true (
 						echo Pushed data from register '%%d' to stack '%%c'
@@ -503,6 +693,30 @@ if %cmd%==* (
 		if /i "%%c"=="$/*" (
 			set v2=true
 		)
+		
+		if /i "%%c"=="=~" (
+			set v2=true
+		)
+		
+		if /i "%%c"=="/=~" (
+			set v2=true
+		)
+		
+		if /i "%%c"=="$#" (
+			set v2=true
+		)
+		
+		if /i "%%c"=="/$#" (
+			set v2=true
+		)
+		
+		if /i "%%c"==".*" (
+			set v2=true
+		)
+		
+		if /i "%%c"=="./*" (
+			set v2=true
+		)
 	
 		if /i %%d==a (
 			set v1=true
@@ -517,6 +731,10 @@ if %cmd%==* (
 		)
 		
 		if /i %%d==z (
+			set v1=true
+		)
+		
+		if /i %%d==h (
 			set v1=true
 		)
 		
@@ -536,6 +754,10 @@ if %cmd%==* (
 			set v2=true
 		)
 		
+		if /i %%e==h (
+			set v2=true
+		)
+		
 		if defined v1 (
 			if defined v2 (
 				set valid=true
@@ -549,7 +771,7 @@ if %cmd%==* (
 			set "cond=%%c"
 			
 			if "!cond!"=="=" (
-				if !R_%%d!==!R_%%e! (
+				if !st.R_%%d!==!st.R_%%e! (
 					for /f "tokens=1* delims= " %%n in ("%%f") do (
 						call :commandHandling "%%n", "%%o"
 					)
@@ -557,7 +779,7 @@ if %cmd%==* (
 			)
 			
 			if "!cond!"=="/=" (
-				if not !R_%%d!==!R_%%e! (
+				if not !st.R_%%d!==!st.R_%%e! (
 					for /f "tokens=1* delims= " %%n in ("%%f") do (
 						call :commandHandling "%%n", "%%o"
 					)
@@ -565,7 +787,7 @@ if %cmd%==* (
 			)
 			
 			if "!cond!"=="$*" (
-				if not "!R_%%d!"=="" (
+				if not "!st.R_%%d!"=="" (
 					for /f "tokens=1* delims= " %%n in ("%%f") do (
 						call :commandHandling "%%n", "%%o"
 					)
@@ -573,7 +795,63 @@ if %cmd%==* (
 			)
 			
 			if "!cond!"=="$/*" (
-				if "!R_%%d!"=="" (
+				if "!st.R_%%d!"=="" (
+					for /f "tokens=1* delims= " %%n in ("%%f") do (
+						call :commandHandling "%%n", "%%o"
+					)
+				)
+			)
+			
+			if "!cond!"=="=~" (
+				if "!st.R_%%d!"=="%%e" (
+					for /f "tokens=1* delims= " %%n in ("%%f") do (
+						call :commandHandling "%%n", "%%o"
+					)
+				)
+			)
+			
+			if "!cond!"=="/=~" (
+				if not "!st.R_%%d!"=="%%e" (
+					for /f "tokens=1* delims= " %%n in ("%%f") do (
+						call :commandHandling "%%n", "%%o"
+					)
+				)
+			)
+			
+			if "!cond!"=="$#" (
+				for /f "delims=0123456789" %%i in ("!st.R_%%d!") do set var=%%i
+				
+				if not defined var (
+					for /f "tokens=1* delims= " %%n in ("%%f") do (
+						call :commandHandling "%%n", "%%o"
+					)
+				)
+				
+				set "var="
+			)
+			
+			if "!cond!"=="/$#" (
+				for /f "delims=0123456789" %%i in ("!st.R_%%d!") do set var=%%i
+				
+				if defined var (
+					for /f "tokens=1* delims= " %%n in ("%%f") do (
+						call :commandHandling "%%n", "%%o"
+					)
+				)
+				
+				set "var="
+			)
+			
+			if "!cond!"==".*" (
+				if defined Stack_%%d.length (
+					for /f "tokens=1* delims= " %%n in ("%%f") do (
+						call :commandHandling "%%n", "%%o"
+					)
+				)
+			)
+			
+			if "!cond!"=="./*" (
+				if not defined Stack_%%d.length (
 					for /f "tokens=1* delims= " %%n in ("%%f") do (
 						call :commandHandling "%%n", "%%o"
 					)
@@ -602,10 +880,14 @@ if %cmd%==:+ (
 		)
 		
 		if defined valid (
-			set num_a=!R_%%d!
-			set num_b=!R_%%e!
+			set num_a=!st.R_%%d!
+			set num_b=!st.R_%%e!
 		
-			set /a "R_%%c=num_a+num_b"
+			set /a "st.R_%%c=num_a+num_b"
+			
+			if !mode_debug!==true (
+				echo Adding values of registers '%%d' and '%%e' together and offloading output to register '%%c'
+			)
 		)
 		
 		set "num_a="
@@ -628,10 +910,14 @@ if %cmd%==:- (
 		)
 		
 		if defined valid (
-			set num_a=!R_%%d!
-			set num_b=!R_%%e!
+			set num_a=!st.R_%%d!
+			set num_b=!st.R_%%e!
 		
-			set /a "R_%%c=num_a+num_b"
+			set /a "st.R_%%c=num_a-num_b"
+			
+			if !mode_debug!==true (
+				echo Subtracting values of registers '%%d' and '%%e' together and offloading output to register '%%c'
+			)
 		)
 		
 		set "num_a="
@@ -654,10 +940,14 @@ if %cmd%==:* (
 		)
 		
 		if defined valid (
-			set num_a=!R_%%d!
-			set num_b=!R_%%e!
+			set num_a=!st.R_%%d!
+			set num_b=!st.R_%%e!
 		
-			set /a "R_%%c=num_a*num_b"
+			set /a "st.R_%%c=num_a*num_b"
+			
+			if !mode_debug!==true (
+				echo Multiplying values of registers '%%d' and '%%e' together and offloading output to register '%%c'
+			)
 		)
 		
 		set "num_a="
@@ -680,10 +970,14 @@ if %cmd%==:/ (
 		)
 		
 		if defined valid (
-			set num_a=!R_%%d!
-			set num_b=!R_%%e!
+			set num_a=!st.R_%%d!
+			set num_b=!st.R_%%e!
 		
-			set /a "R_%%c=num_a/num_b"
+			set /a "st.R_%%c=num_a/num_b"
+			
+			if !mode_debug!==true (
+				echo Dividing values of registers '%%d' and '%%e' together and offloading output to register '%%c'
+			)
 		)
 		
 		set "num_a="
@@ -709,7 +1003,7 @@ if "%cmd%"=="F" (
 			)
 			
 			if defined valid (
-				echo !R_%%d!>>%%e
+				echo !st.R_%%d!>>%%e
 			)
 		)
 		
@@ -740,9 +1034,13 @@ REM Loop instructions
 if /i %cmd%==L (
 	for /f "tokens=1,2,3,4 delims= " %%c in ("%data%") do (
 		if /i %%c==S (
+			if !mode_debug!==true (
+				echo Looping through stack '%%d'
+			)
+		
 			for /l %%n in (0, 1, !Stack_%%d.length!) do (
 				if not "!Stack_%%d[%%n]!"=="" (
-					set "R_z=!Stack_%%d[%%n]!"
+					set "st.R_z=!Stack_%%d[%%n]!"
 					
 					call :loop
 				)
@@ -750,9 +1048,33 @@ if /i %cmd%==L (
 		)
 		
 		if /i %%c==N (
-			for /l %%n in (%%d, %%e, %%f) do (
+			if !mode_debug!==true (
+				echo Looping through integer range, start '%%d', step '%%e', end '%%f'
+			)
+			
+			set /a end=%%f
+			
+			if %%f==a (
+				set /a end=!st.R_%%f!
+			)
+			
+			if %%f==b (
+				set /a end=!st.R_%%f!
+			)
+			
+			if %%f==c (
+				set /a end=!st.R_%%f!
+			)
+			
+			if %%f==h (
+				set /a end=!st.R_%%f!
+			)
+		
+			for /l %%n in (%%d, %%e, !end!) do (
 				call :loop
 			)
+			
+			set "end="
 		)
 		
 		if /i %%c==W (
@@ -762,7 +1084,7 @@ if /i %cmd%==L (
 				set delim=%%e
 			)
 			
-			for /f "tokens=1,2 delims=," %%A in ("!R_%%f!") do (
+			for /f "tokens=1,2 delims=," %%A in ("!st.R_%%f!") do (
 				for %%n in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
 					set char=%%n
 					set "val=^%%^%char%"
@@ -780,8 +1102,16 @@ REM Misc instructions
 if %cmd%==- (
 	for /f "tokens=1,2 delims= " %%c in ("%data%") do (
 		if %%d==r (
-			set "R_%%c="
+			if !mode_debug!==true (
+				echo Pulling item within register '%%c'
+			)
+		
+			set "st.R_%%c="
 		) else (
+			if !mode_debug!==true (
+				echo Clearing stack '%%c'
+			)
+		
 			call :clear "Stack_%%c"
 		)
 	)
@@ -794,7 +1124,7 @@ goto:EOF
 
 
 :loop
-for /l %%m in (!line!, 1, !Stack_x.length!) do (
+for /l %%m in (!st.line!, 1, !Stack_x.length!) do (
 	set code=!Stack_x[%%m]!
 
 	if "!code!"=="}" (
@@ -808,6 +1138,10 @@ for /l %%m in (!line!, 1, !Stack_x.length!) do (
 	)
 
 	if defined blockmode (
+		set st.title=ST - Script: %ST_SCRIPT% - Executing - Line: %%m
+
+		title !st.title!
+	
 		for /f "tokens=1* delims= " %%o in ("!code!") do (
 			call :commandHandling "%%o", "%%p"
 		)
